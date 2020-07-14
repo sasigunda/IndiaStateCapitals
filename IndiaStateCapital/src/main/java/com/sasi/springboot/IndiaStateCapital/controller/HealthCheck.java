20866a0939cbebc092e2d091fc815c7cf0271ff8
sasi <sasi@localhost>
2020-07-14 09:03:45 -0400

    Commit - Healthcheck

--- IndiaStateCapital/src/main/java/com/sasi/springboot/IndiaStateCapital/controller/HealthCheck.java
+++ IndiaStateCapital/src/main/java/com/sasi/springboot/IndiaStateCapital/controller/HealthCheck.java
@@ -1,14 +1,15 @@
 package com.sasi.springboot.IndiaStateCapital.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheck {
 	
-	@RequestMapping("/heatlcheck")
-	public String healthCheck() {
-		return "Im  up and running";
+	@RequestMapping("/health")
+	public HttpStatus healthCheck() {
+		return HttpStatus.OK;
 	}
 
 }
